510eec93ec5a5f39d6e1b2a1692dd2869415c340
Nathan Lecourt <nathan.lecourt@gmail.com>
2024-06-19 07:34:12 +0200

    Fix letter to pos mapping and improve helper

--- helper.lua
+++ helper.lua
@@ -1,35 +1,3 @@
-local char_pos_map = {
-	["T"] = "POS_LH_C1R3",
-	["G"] = "POS_LH_C1R4",
-	["B"] = "POS_LH_C1R5",
-	["R"] = "POS_LH_C2R3",
-	["F"] = "POS_LH_C2R4",
-	["V"] = "POS_LH_C2R5",
-	["E"] = "POS_LH_C3R3",
-	["D"] = "POS_LH_C3R4",
-	["C"] = "POS_LH_C3R5",
-	["W"] = "POS_LH_C4R3", -- Z
-	["S"] = "POS_LH_C4R4",
-	["X"] = "POS_LH_C4R5",
-	["Q"] = "POS_LH_C5R3", -- A
-	["A"] = "POS_LH_C5R4", -- Q
-	["Z"] = "POS_LH_C5R5", -- W
-	["Y"] = "POS_RH_C1R3",
-	["H"] = "POS_RH_C1R4",
-	["N"] = "POS_RH_C1R5",
-	["U"] = "POS_RH_C2R3",
-	["J"] = "POS_RH_C2R4",
-	-- ["M"] = "POS_RH_C2R5", -- ,
-	["I"] = "POS_RH_C3R3",
-	["K"] = "POS_RH_C3R4",
-	-- [","] = "POS_RH_C3R5",
-	["O"] = "POS_RH_C4R3",
-	["L"] = "POS_RH_C4R4",
-	-- [""] = "POS_RH_C4R5",
-	["P"] = "POS_RH_C5R3",
-	[";"] = "POS_RH_C5R4", -- M
-}
-
 local word_maps = {
 	{"the", "TE"},
 	{"be", "BE"},
@@ -47,6 +15,39 @@ local word_maps = {
 	{"with", "WITH"},
 	{"he", "HE"},
 	{"as", "AS"}
+}
+
+local char_pos_map = {
+	["T"] = "POS_LH_C1R3",
+	["G"] = "POS_LH_C1R4",
+	["B"] = "POS_LH_C1R5",
+	["R"] = "POS_LH_C2R3",
+	["F"] = "POS_LH_C2R4",
+	["V"] = "POS_LH_C2R5",
+	["E"] = "POS_LH_C3R3",
+	["D"] = "POS_LH_C3R4",
+	["C"] = "POS_LH_C3R5",
+	["Z"] = "POS_LH_C4R3",
+	["S"] = "POS_LH_C4R4",
+	["X"] = "POS_LH_C4R5",
+	["A"] = "POS_LH_C5R3",
+	["Q"] = "POS_LH_C5R4",
+	["W"] = "POS_LH_C5R5",
+	["Y"] = "POS_RH_C1R3",
+	["H"] = "POS_RH_C1R4",
+	["N"] = "POS_RH_C1R5",
+	["U"] = "POS_RH_C2R3",
+	["J"] = "POS_RH_C2R4",
+	-- [""] = "POS_RH_C2R5",
+	["I"] = "POS_RH_C3R3",
+	["K"] = "POS_RH_C3R4",
+	-- [""] = "POS_RH_C3R5",
+	["O"] = "POS_RH_C4R3",
+	["L"] = "POS_RH_C4R4",
+	-- [""] = "POS_RH_C4R5",
+	["P"] = "POS_RH_C5R3",
+	["M"] = "POS_RH_C5R4",
+	-- [""] = "POS_RH_C5R5",
 }
 
 local macro_tmpl = [[
@@ -118,14 +119,31 @@ local word_to_combo = function(key, value)
 	return combo_tmpl:format(key, chars_to_positions(value), key)
 end
 
-for _, word in ipairs(word_maps) do
-	local key = word[1]
-	local value = word[2]
-	print(word_to_macro(key, value))
-end
-for _, word in ipairs(word_maps) do
-	local key = word[1]
-	local value = word[2]
-	print(word_to_combo(key, value))
+--- @return table, table
+local generate_macros_and_combos = function()
+	local macros = {}
+	local combos = {}
+	for _, word in ipairs(word_maps) do
+		local key = word[1]
+		local value = word[2]
+		table.insert(macros, word_to_macro(key, value))
+		table.insert(combos, word_to_combo(key, value))
+	end
+	return macros, combos
 end
 
+local generate_in_nvim_split = function()
+	local macros, combos = generate_macros_and_combos()
+	local macros_str = table.concat(macros, "\n")
+	local combos_str = table.concat(combos, "\n")
+	local macro_lines = vim.split(macros_str, "\n")
+	local combo_lines = vim.split(combos_str, "\n")
+
+	vim.cmd("vsplit")
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_win_set_buf(0, buf)
+	vim.api.nvim_buf_set_lines(buf, -1, -1, false, macro_lines)
+	vim.api.nvim_buf_set_lines(buf, -1, -1, false, combo_lines)
+end
+
+generate_in_nvim_split()
